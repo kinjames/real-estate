ed9d49e297066f9faba8b18a0b2b4429a5181b59
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-04 16:44:50 +0000

    update

--- apps/js/script.js
+++ apps/js/script.js
@@ -12,9 +12,36 @@ menuBtn.addEventListener('click', ()=>{
     else{
         menuBtn.classList.remove('open');
         mobileMenu.classList.add('hide');
-        body.classList.remove('noscrol')
+        body.classList.remove('noscroll')
 
 
     }
 
 })
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 30,
+      },
+    },
+  });
