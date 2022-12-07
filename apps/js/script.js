0e56e000aaf090ea86f167e61d0d8782ad3301a9
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-07 23:17:16 +0000

    update all

--- apps/js/script.js
+++ apps/js/script.js
@@ -48,3 +48,37 @@ var swiper = new Swiper(".swiper", {
       },
     },
   });
+
+  //splide
+  var main = new Splide( '#main-slider', {
+    type       : 'fade',
+    heightRatio: 0.5,
+    pagination : false,
+    arrows     : false,
+    cover      : true,
+  } );
+  
+  var thumbnails = new Splide( '#thumbnail-slider', {
+    rewind          : true,
+    fixedWidth      : 104,
+    fixedHeight     : 58,
+    isNavigation    : true,
+    gap             : 10,
+    focus           : 'center',
+    pagination      : false,
+    cover           : true,
+    dragMinThreshold: {
+      mouse: 4,
+      touch: 10,
+    },
+    breakpoints : {
+      640: {
+        fixedWidth  : 66,
+        fixedHeight : 38,
+      },
+    },
+  } );
+  
+  main.sync( thumbnails );
+  main.mount();
+  thumbnails.mount();
