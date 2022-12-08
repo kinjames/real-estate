868968b5743736f2035b7c4d0df764696f5a7d6c
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-08 11:48:29 +0000

    update

--- apps/js/script.js
+++ apps/js/script.js
@@ -19,6 +19,8 @@ menuBtn.addEventListener('click', ()=>{
 
 })
 
+//Swiper.js
+
 var swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -49,36 +51,3 @@ var swiper = new Swiper(".swiper", {
     },
   });
 
-  //splide
-  var main = new Splide( '#main-slider', {
-    type       : 'fade',
-    heightRatio: 0.5,
-    pagination : false,
-    arrows     : false,
-    cover      : true,
-  } );
-  
-  var thumbnails = new Splide( '#thumbnail-slider', {
-    rewind          : true,
-    fixedWidth      : 104,
-    fixedHeight     : 58,
-    isNavigation    : true,
-    gap             : 10,
-    focus           : 'center',
-    pagination      : false,
-    cover           : true,
-    dragMinThreshold: {
-      mouse: 4,
-      touch: 10,
-    },
-    breakpoints : {
-      640: {
-        fixedWidth  : 66,
-        fixedHeight : 38,
-      },
-    },
-  } );
-  
-  main.sync( thumbnails );
-  main.mount();
-  thumbnails.mount();
