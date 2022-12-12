a5f958f42f069f120e128d3a442651d9006c6200
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-12 21:37:58 +0000

    update

--- apps/js/pagination.js
+++ apps/js/pagination.js
@@ -1,19 +1,30 @@
   //pagination
-  let card = '<div class="property__cards flex">'+'<div class="property__cards-img-icons">'+'<div class="property__cards-img">'+'<img src="./images/img1.jpg" alt="">'+'</div>'+'<div class="property__cards-icons flex">'+'<div class="swiper__icon flex flex-ai-c">' + '<i class="fas fa-bed"></i>'+ '<span>2</span>'+'</div>' + '<div class="swiper__icon flex flex-ai-c">'+'<i class="fas fa-bath"></i>'+'<span>2</span>'+'</div>'+'<div class="swiper__icon flex flex-ai-c">'+'<i class="fas fa-car"></i>'+'<span>1</span>'+'</div>'+'</div>'+'</div>'+'<div class="property__cards-texts">'+'<h3>&dollar;400 / WEEK</h3>'+'<p class="property__cards-small__text">'+'Haberfield NSW 2045'+'</p>'+'<p class="property__cards-para">'+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit animi molestiae libero odio magni fug. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eum mollitia,'+'</p>'+'</div>'+'</div>';
+  let card = '<div class="property__cards flex">'+'<div class="property__cards-img-icons">'+'<div class="property__cards-img">'+'<img src="./images/img1.jpg" alt="" class="ima">'+'</div>'+'<div class="property__cards-icons flex">'+'<div class="swiper__icon flex flex-ai-c">' + '<i class="fas fa-bed"></i>'+ '<span>2</span>'+'</div>' + '<div class="swiper__icon flex flex-ai-c">'+'<i class="fas fa-bath"></i>'+'<span>2</span>'+'</div>'+'<div class="swiper__icon flex flex-ai-c">'+'<i class="fas fa-car"></i>'+'<span>1</span>'+'</div>'+'</div>'+'</div>'+'<div class="property__cards-texts">'+'<h3>&dollar;400 / WEEK</h3>'+'<p class="property__cards-small__text">'+'Haberfield NSW 2045'+'</p>'+'<p class="property__cards-para">'+'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit odit animi molestiae libero odio magni fug. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi eum mollitia,'+'</p>'+'</div>'+'</div>';
+
+
 
   let list =document.querySelector('.property__cards-list')
 
   const listArray = []
-  for (let i=0; i<5; i++) {
+  for (let i=0; i<10; i++) {
     listArray.push(card)
 }
 
+
 list.innerHTML= listArray
+let images = document.querySelectorAll('.ima')
+let prices = document.querySelector('.price')
+let smallText = document.querySelector('.property__cards-small__text')
+let desc = document.querySelector('.property__cards-para')
 
 console.log(listArray)
 
 fetch ('./apps/js/data.json')
     .then(response => response.json())
     .then(data => {
-        console.log(data[1])
-    })
+        for (let j = 0; j < 10;j++){
+            images.forEach((j)=>{
+                images[j].innerHTML = data[j].image
+            })
+        }
+})
