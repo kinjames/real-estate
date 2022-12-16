3b37ff8821ceff055cbb204feb899d85da9ab4f4
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-16 20:27:32 +0000

    update

--- apps/js/pagination.js
+++ apps/js/pagination.js
@@ -29,74 +29,74 @@
 //         }
 // })
 
-function getPageList(totalPages, page, maxLength){
-    function range(start, end){
-      return Array.from(Array(end - start + 1), (_, i) => i + start);
-    }
+// function getPageList(totalPages, page, maxLength){
+//     function range(start, end){
+//       return Array.from(Array(end - start + 1), (_, i) => i + start);
+//     }
   
-    var sideWidth = maxLength < 9 ? 1 : 2;
-    var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
-    var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+//     var sideWidth = maxLength < 9 ? 1 : 2;
+//     var leftWidth = (maxLength - sideWidth * 2 - 3) >> 1;
+//     var rightWidth = (maxLength - sideWidth * 2 - 3) >> 1;
   
-    if(totalPages <= maxLength){
-      return range(1, totalPages);
-    }
+//     if(totalPages <= maxLength){
+//       return range(1, totalPages);
+//     }
   
-    if(page <= maxLength - sideWidth - 1 - rightWidth){
-      return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
-    }
+//     if(page <= maxLength - sideWidth - 1 - rightWidth){
+//       return range(1, maxLength - sideWidth - 1).concat(0, range(totalPages - sideWidth + 1, totalPages));
+//     }
   
-    if(page >= totalPages - sideWidth - 1 - rightWidth){
-      return range(1, sideWidth).concat(0, range(totalPages- sideWidth - 1 - rightWidth - leftWidth, totalPages));
-    }
+//     if(page >= totalPages - sideWidth - 1 - rightWidth){
+//       return range(1, sideWidth).concat(0, range(totalPages- sideWidth - 1 - rightWidth - leftWidth, totalPages));
+//     }
   
-    return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
-  }
+//     return range(1, sideWidth).concat(0, range(page - leftWidth, page + rightWidth), 0, range(totalPages - sideWidth + 1, totalPages));
+//   }
   
-  $(function(){
-    var numberOfItems = $(".card-content .card").length;
-    var limitPerPage = 3; //How many card items visible per a page
-    var totalPages = Math.ceil(numberOfItems / limitPerPage);
-    var paginationSize = 7; //How many page elements visible in the pagination
-    var currentPage;
+//   $(function(){
+//     var numberOfItems = $(".card-content .card").length;
+//     var limitPerPage = 3; //How many card items visible per a page
+//     var totalPages = Math.ceil(numberOfItems / limitPerPage);
+//     var paginationSize = 7; //How many page elements visible in the pagination
+//     var currentPage;
   
-    function showPage(whichPage){
-      if(whichPage < 1 || whichPage > totalPages) return false;
+//     function showPage(whichPage){
+//       if(whichPage < 1 || whichPage > totalPages) return false;
   
-      currentPage = whichPage;
+//       currentPage = whichPage;
   
-      $(".card-content .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
+//       $(".card-content .card").hide().slice((currentPage - 1) * limitPerPage, currentPage * limitPerPage).show();
   
-      $(".pagination li").slice(1, -1).remove();
+//       $(".pagination li").slice(1, -1).remove();
   
-      getPageList(totalPages, currentPage, paginationSize).forEach(item => {
-        $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
-        .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
-        .attr({href: "javascript:void(0)"}).text(item || "...")).insertBefore(".next-page");
-      });
+//       getPageList(totalPages, currentPage, paginationSize).forEach(item => {
+//         $("<li>").addClass("page-item").addClass(item ? "current-page" : "dots")
+//         .toggleClass("active", item === currentPage).append($("<a>").addClass("page-link")
+//         .attr({href: "javascript:void(0)"}).text(item || "...")).insertBefore(".next-page");
+//       });
   
-      $(".previous-page").toggleClass("disable", currentPage === 1);
-      $(".next-page").toggleClass("disable", currentPage === totalPages);
-      return true;
-    }
+//       $(".previous-page").toggleClass("disable", currentPage === 1);
+//       $(".next-page").toggleClass("disable", currentPage === totalPages);
+//       return true;
+//     }
   
-    $(".pagination").append(
-      $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Prev")),
-      $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Next"))
-    );
+//     $(".pagination").append(
+//       $("<li>").addClass("page-item").addClass("previous-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Prev")),
+//       $("<li>").addClass("page-item").addClass("next-page").append($("<a>").addClass("page-link").attr({href: "javascript:void(0)"}).text("Next"))
+//     );
   
-    $(".main__blog-page-card").show();
-    showPage(1);
+//     $(".main__blog-page-card").show();
+//     showPage(1);
   
-    $(document).on("click", ".pagination li.current-page:not(.active)", function(){
-      return showPage(+$(this).text());
-    });
+//     $(document).on("click", ".pagination li.current-page:not(.active)", function(){
+//       return showPage(+$(this).text());
+//     });
   
-    $(".next-page").on("click", function(){
-      return showPage(currentPage + 1);
-    });
+//     $(".next-page").on("click", function(){
+//       return showPage(currentPage + 1);
+//     });
   
-    $(".previous-page").on("click", function(){
-      return showPage(currentPage - 1);
-    });
-  });
+//     $(".previous-page").on("click", function(){
+//       return showPage(currentPage - 1);
+//     });
+//   });
