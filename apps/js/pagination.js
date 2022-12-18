cc39d85c46e883158ac2fe6e9c500c0030023c53
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-18 22:34:38 +0000

    update all

--- apps/js/pagination.js
+++ apps/js/pagination.js
@@ -6,6 +6,7 @@ let blogHeader = document.querySelectorAll('.blog__subheader');
 let paginateNumbers = document.querySelectorAll('.paginate__numbers')
 const numOne = document.querySelector('.one')
 const numTwo = document.querySelector('.two')
+const numThree = document.querySelector('.three')
 
 
 
@@ -70,9 +71,40 @@ function paginate__3() {
             blogHeader[i].textContent = arr[i]?.title
         })
         console.log(arr[1])
-        numTwo.classList.add('active-page')
+        numThree.classList.add('active-page')
     })
 }
+
+paginateNumbers.forEach(paginate__numbers =>{
+    paginate__numbers.addEventListener('click', (e)=>{
+        const styles = e.currentTarget.classList;
+        if(styles.contains('one')){
+            numOne.classList.add('active-page')          
+            numThree.classList.remove('active-page')          
+            numTwo.classList.remove('active-page')
+            
+            paginate__1();
+        }else if(styles.contains('two')){
+            numOne.classList.remove('active-page')          
+            numTwo.classList.add('active-page')          
+            numThree.classList.remove('active-page')  
+            
+            paginate__2();
+        }
+        else{
+            numOne.classList.remove('active-page')          
+            numTwo.classList.remove('active-page')          
+            numThree.classList.add('active-page')
+            
+            paginate__3()
+        }
+    })
+})
+
+
+window.onload = paginate__1();
+window.onload = numOne.classList.add('active-page') 
+
 
 
 
@@ -83,22 +115,9 @@ function paginate__3() {
 
 // reqData();
 
-window.onload = paginate__3();
 
-// paginateNumbers.forEach(paginate__numbers =>{
-//     paginate__numbers.addEventListener('click', (e)=>{
-//         const styles = e.currentTarget.classList;
-//         if(styles.contains('one')){
-//             let arr = [];
-//             arr.push(data.splice(9,9))
-//             blogCardText.forEach((e,i)=>{
-//                 blogCardText[i].textContent = data[i].summary
-//             })
-//             numOne.classList.add('active-page')
-            
-//         }
-//     })
-// })
+
+
 
 
 // fetch('./apps/js/blog.json')
