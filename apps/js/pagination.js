4851d6bc3c4c80466f39ad915346914c42ddd173
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-18 20:25:02 +0000

    update all

--- apps/js/pagination.js
+++ apps/js/pagination.js
@@ -1,8 +1,65 @@
 let currentPage = 1;
 let mainCardList = document.querySelector('.main__blog-page-card-list');
-let blogCard = document.querySelectorAll('.blog__text');
+let blogCardText = document.querySelectorAll('.blog__text');
 let blogImage = document.querySelectorAll('.blogImagesJs');
 let blogHeader = document.querySelectorAll('.blog__subheader');
+let paginateNumbers = document.querySelectorAll('.paginate__numbers')
+
+
+
+
+
+function reqData() {
+    fetch('./apps/js/blog.json')
+    .then(response => response.json())
+    .then(data => {
+
+        // blogCardText.forEach((e,i)=>{
+        //     blogCardText[i].textContent = data[i].summary
+        // })
+        blogHeader.forEach((e,i)=>{
+            blogHeader[i].textContent = data[i].title
+        })
+        blogImage.forEach((e,i)=>{
+            blogImage[i].src = data[i].image
+        })
+        paginateNumbers.forEach(paginate__numbers =>{
+            paginate__numbers.addEventListener('click', (e,i)=>{
+                const styles = e.currentTarget.classList;
+                
+        
+                if(styles.contains('one')){
+                    let arr = [];
+                    arr.push(data.splice(0,9))
+                    for (let i = 0; i < arr.length; i++) {
+                        blogCardText[i].textContent = data[i].summary  
+                    }
+                    console.log('num1');
+                }
+            })
+        })
+
+
+        // for (let i = 0; i < data.length; i++) {
+        //     console.log(data[i])
+        //     blogCardText[i].classList = data[i]
+        //     // listArray.push(blogCard);
+        //     // console.log(listArray)
+        //     // mainCardList.innerHTML = listArray
+        // }
+    })
+}
+
+reqData();
+
+
+// fetch('./apps/js/blog.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data.splice(0,9));
+//     })
+
+
 // const listArray = [];
 // let blogCard = '<li class="main__blog-page-card blog__card">'
 // + '<div class="blog__card-image">'+ 
@@ -22,36 +79,3 @@ let blogHeader = document.querySelectorAll('.blog__subheader');
 // '</i>'+
 // '</a>'
 // +'</li>'
-
-function reqData() {
-    fetch('./apps/js/blog.json')
-    .then(response => response.json())
-    .then(data => {
-
-        blogCard.forEach((e,i)=>{
-            blogCard[i].textContent = data[i].summary
-        })
-        blogHeader.forEach((e,i)=>{
-            blogHeader[i].textContent = data[i].title
-        })
-        blogImage.forEach((e,i)=>{
-            blogImage[i].src = data[i].image
-        })
-        // for (let i = 0; i < data.length; i++) {
-        //     console.log(data[i])
-        //     blogCard[i].classList = data[i]
-        //     // listArray.push(blogCard);
-        //     // console.log(listArray)
-        //     // mainCardList.innerHTML = listArray
-        // }
-    })
-}
-
-reqData();
-
-
-// fetch('./apps/js/blog.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data.splice(0,9));
-//     })
