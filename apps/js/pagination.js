d1231018459994092513141d887b250706bf0f2d
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-19 20:47:35 +0000

    update all

--- apps/js/pagination.js
+++ apps/js/pagination.js
@@ -13,15 +13,15 @@ const mainBlogPage = document.querySelector('.main__blog-page')
 const subBlogPage = document.querySelector('.blog__main')
 
 //
-const subHeading = document.querySelector('.blog__subheading');
-const subImage = document.querySelector('.subBlogImage');
-const para1 = document.querySelector('.para_1');
-const para2 = document.querySelector('.para_2');
-const para3 = document.querySelector('.para_3');
-const para4 = document.querySelector('.para_4');
-const subTitle = document.querySelector('.blog__title');
-const authorName = document.querySelector('.authorName');
-const dateNum = document.querySelector('.dateNum');
+const subHeading = document.querySelectorAll('.blog__subheading');
+const subImage = document.querySelectorAll('.subBlogImage');
+const para1 = document.querySelectorAll('.para_1');
+const para2 = document.querySelectorAll('.para_2');
+const para3 = document.querySelectorAll('.para_3');
+const para4 = document.querySelectorAll('.para_4');
+const subTitle = document.querySelectorAll('.blog__title');
+const authorName = document.querySelectorAll('.authorName');
+const dateNum = document.querySelectorAll('.dateNum');
 
 
 
@@ -41,6 +41,34 @@ function paginate__1() {
 
         blogHeader.forEach((e,i)=>{
             blogHeader[i].textContent = data[i].title
+        })
+
+        subHeading.forEach((e,i)=>{
+            subHeading[i].textContent = data[i].subheading
+        })
+        subImage.forEach((e,i)=>{
+            subImage[i].src = data[i].image
+        })
+        subTitle.forEach((e,i)=>{
+            subTitle[i].textContent = data[i].title
+        })
+        authorName.forEach((e,i)=>{
+            authorName[i].textContent = data[i].writer
+        })
+        dateNum.forEach((e,i)=>{
+            dateNum[i].textContent = data[i].date
+        })
+        para1.forEach((e,i)=>{
+            para1[i].textContent = data[i].para1
+        })
+        para2.forEach((e,i)=>{
+            para2[i].textContent = data[i].para2
+        })
+        para3.forEach((e,i)=>{
+            para3[i].textContent = data[i].para3
+        })
+        para4.forEach((e,i)=>{
+            para4[i].textContent = data[i].para4
         })
     })
 
@@ -62,6 +90,34 @@ function paginate__2() {
         blogHeader.forEach((e,i)=>{
             blogHeader[i].textContent = arr[i]?.title
         })
+
+        subHeading.forEach((e,i)=>{
+            subHeading[i].textContent = arr[i]?.subheading
+        })
+        subImage.forEach((e,i)=>{
+            subImage[i].src = arr[i]?.image
+        })
+        subTitle.forEach((e,i)=>{
+            subTitle[i].textContent = arr[i]?.title
+        })
+        authorName.forEach((e,i)=>{
+            authorName[i].textContent = arr[i]?.writer
+        })
+        dateNum.forEach((e,i)=>{
+            dateNum[i].textContent = arr[i]?.date
+        })
+        para1.forEach((e,i)=>{
+            para1[i].textContent = arr[i]?.para1
+        })
+        para2.forEach((e,i)=>{
+            para2[i].textContent = arr[i]?.para2
+        })
+        para3.forEach((e,i)=>{
+            para3[i].textContent = arr[i]?.para3
+        })
+        para4.forEach((e,i)=>{
+            para4[i].textContent = arr[i]?.para4
+        })
     })
 }
 function paginate__3() {
@@ -81,8 +137,49 @@ function paginate__3() {
         blogHeader.forEach((e,i)=>{
             blogHeader[i].textContent = arr[i]?.title
         })
+
+        subHeading.forEach((e,i)=>{
+            subHeading[i].textContent = arr[i]?.subheading
+        })
+        subImage.forEach((e,i)=>{
+            subImage[i].src = arr[i]?.image
+        })
+        subTitle.forEach((e,i)=>{
+            subTitle[i].textContent = arr[i]?.title
+        })
+        authorName.forEach((e,i)=>{
+            authorName[i].textContent = arr[i]?.writer
+        })
+        dateNum.forEach((e,i)=>{
+            dateNum[i].textContent = arr[i]?.date
+        })
+        para1.forEach((e,i)=>{
+            para1[i].textContent = arr[i]?.para1
+        })
+        para2.forEach((e,i)=>{
+            para2[i].textContent = arr[i]?.para2
+        })
+        para3.forEach((e,i)=>{
+            para3[i].textContent = arr[i]?.para3
+        })
+        para4.forEach((e,i)=>{
+            para4[i].textContent = arr[i]?.para4
+        })
     })
 }
+
+link.forEach(link=>{
+    link.addEventListener('click', ()=>{
+        if(subBlogPage.classList.contains('hide')){
+            subBlogPage.classList.remove('hide');
+            mainBlogPage.classList.add('hide');
+            paginationContainer.classList.add('hide');
+            paginate__1();
+            paginate__2();
+            paginate__3();
+        }
+    })
+})
 
 paginateNumbers.forEach(paginate__numbers =>{
     paginate__numbers.addEventListener('click', (e)=>{
@@ -111,30 +208,13 @@ paginateNumbers.forEach(paginate__numbers =>{
 })
 
 
+
 window.onload = paginate__1();
 window.onload = numOne.classList.add('active-page');
 
-function blog(){
-    fetch('./apps/js/blog.json')
-    .then(response => response.json())
-    .then(data => {
-        for (let i = 0; i < data.length; i++) {
-            subHeading.innerHTML = data[i].subheading;
-            console.log(data[i].subheading)
-        }
-    })
-}
 
-link.forEach(link=>{
-    link.addEventListener('click', ()=>{
-        if(subBlogPage.classList.contains('hide')){
-            subBlogPage.classList.remove('hide');
-            mainBlogPage.classList.add('hide');
-            paginationContainer.classList.add('hide');
 
-        }
-    })
-})
+
 
 
 
