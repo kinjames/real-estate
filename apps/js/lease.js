d57a3f4d6e5c7c26c64f0b595a755894fe02734a
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2022-12-28 21:51:03 +0000

    update

---
+++ apps/js/lease.js
@@ -0,0 +1,13 @@
+let card = '<div class="lease__card flex flex-jc-sb">'+ '<div class="lease__card-image-icons">'+  +'</div>' +'</div'
+
+let lease__card = document.querySelector('.lease__card');
+let property__card = document.querySelector('.property__cards-list');
+
+for (let i = 0; i < 10; i++) {
+
+    let myArr = [];
+    myArr.push(property__card[i])
+    console.log(myArr);
+//    property__card.innerHTML = lease__card[i]
+    
+}
