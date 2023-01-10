14f174e88f1f62c8416832615eb9caf284205b45
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-01-10 20:26:23 +0000

    update

--- apps/js/lease.js
+++ apps/js/lease.js
@@ -1,2 +1,16 @@
 
 
+let leaseCard = document.querySelectorAll('.lease__card');
+let cardList = document.querySelector('.property__cards-list');
+
+
+
+
+leaseCard.forEach(card=>{
+    card.addEventListener('click',()=>{
+        console.log('item clicked');
+        cardList.classList.addn('hide');
+    })
+})
+
+
