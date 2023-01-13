c116271ae5ea3a4983d81a444275f498dc5c2a97
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-01-13 23:31:05 +0000

    update

--- apps/js/lease.js
+++ apps/js/lease.js
@@ -9,13 +9,30 @@ let bath = document.querySelectorAll('.bath');
 let storey = document.querySelectorAll('.storey');
 let swiperBtn = document.querySelectorAll('.swiper__btn-2');
 let amount = document.querySelectorAll('.swiper__btn-1');
+let bigImage = document.querySelectorAll('.big-image');
+let date = document.querySelector('.date');
 
 
 swiperBtn.forEach(btn =>{
     btn.addEventListener('click',()=>{
-
+        images()
     })
 });
+
+function images(){
+    fetch('./apps/js/lease.json')
+    .then(response => response.json())
+    .then(data =>{
+        for(i = 0; i<data.length; i++){
+            for (let j = 0; j < 5; j++) {
+                    bigImage[j].src = data[i].images[j];
+                    date[i].innerHTML = data[i].date
+            }
+        }
+    })
+}
+
+
 
 function mainPage(){
     fetch('./apps/js/lease.json')
