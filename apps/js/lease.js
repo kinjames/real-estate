14c406096c1303fa4f948ee90e9f8a587260e911
James Kofi Myers <95756936+kinjames@users.noreply.github.com>
2023-01-13 22:16:48 +0000

    update

--- apps/js/lease.js
+++ apps/js/lease.js
@@ -1,6 +1,49 @@
 let leaseCard = document.querySelectorAll('.lease__card');
 let cardList = document.querySelector('.property__cards-list');
 
+let swiperSlide = document.querySelectorAll('.swiper-slide');
+let swiperHeader = document.querySelectorAll('.swiper__header');
+let swiperText = document.querySelectorAll('.swiper__text');
+let bed = document.querySelectorAll('.bed');
+let bath = document.querySelectorAll('.bath');
+let storey = document.querySelectorAll('.storey');
+let swiperBtn = document.querySelectorAll('.swiper__btn-2');
+let amount = document.querySelectorAll('.swiper__btn-1');
+
+
+swiperBtn.forEach(btn =>{
+    btn.addEventListener('click',()=>{
+
+    })
+});
+
+function mainPage(){
+    fetch('./apps/js/lease.json')
+    .then(response => response.json())
+    .then(data =>{
+        for(i = 0; i<data.length; i++){
+            swiperText[i].innerHTML = data[i].header;
+            swiperHeader[i].innerHTML = data[i].place;
+            bed[i].innerHTML = data[i].icons.bed;
+            bath[i].innerHTML = data[i].icons.shower;
+            storey[i].innerHTML = data[i].icons.storey;
+            amount[i].innerHTML = data[i].price;
+        }
+    });
+}
+fetch('./apps/js/lease.json')
+    .then(response => response.json())
+    .then(data =>{
+        for(i = 0; i<data.length; i++){
+            swiperText[i].innerHTML = data[i].header;
+            swiperHeader[i].innerHTML = data[i].place;
+            bed[i].innerHTML = data[i].icons.bed;
+            bath[i].innerHTML = data[i].icons.shower;
+            storey[i].innerHTML = data[i].icons.storey;
+            amount[i].innerHTML = data[i].price;
+        }
+    });
+
 
 
 
